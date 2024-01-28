a4b6cd43fc863fc76f30d5db831927217c041199
Lena L. <kisuni@gmail.com>
2024-01-28 23:26:42 +0300

    ready

--- src/config.ts
+++ src/config.ts
@@ -3,19 +3,19 @@ export const CONFIG = {
         port: +(process.env.API_PORT ?? 8000),
         host: process.env.API_HOST
     },
-    db: {
-        name: 'users',
-        connector: 'postgresql',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    },
+    // db: {
+    //     name: 'users',
+    //     connector: 'postgresql',
+    //     host: process.env.DB_HOST,
+    //     port: process.env.DB_PORT,
+    //     user: process.env.DB_USER,
+    //     password: process.env.DB_PASSWORD,
+    //     database: process.env.DB_DATABASE
+    // },
     balance: {
-        host: "http://127.0.0.1:3001/balance/add",
+        //host: "http://127.0.0.1:3001/balance/add",
         //host: "http://payment-service.default.svc.cluster.local/balance/reserve"
-        //host: process.env.BALANCE_HOST ?? "",
+        host: process.env.BALANCE_HOST ?? "",
     },
     trace: true
 }
